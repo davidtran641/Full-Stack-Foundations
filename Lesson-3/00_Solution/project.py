f96d691ba3f417dd4d9cc2c65ffad9686528b1d2
davidtran <davidtran641@gmail.com>
2020-05-16 23:57:40 +0800

    (FEAT) Add route for restaurant

--- Lesson-3/00_Solution/project.py
+++ Lesson-3/00_Solution/project.py
@@ -13,9 +13,9 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
@@ -27,6 +27,18 @@ def HelloWorld():
         output += '</br>'
         output += '</br>'
     return output
+
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
